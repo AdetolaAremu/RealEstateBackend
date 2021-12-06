1c0255ae108f30ea5b7c8d296894f86201d3a5e4
Adetola <kingdaniel101@ikooba.com>
2021-12-06 06:53:16 +0100

    estate types and the likes

--- database/migrations/2021_12_05_150125_create_posts_table.php
+++ database/migrations/2021_12_05_150125_create_posts_table.php
@@ -17,10 +17,14 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('text');
             $table->string('address');
+            $table->decimal('price');
+            $table->foreignId('type')->references('id')->on('estate_types');
             $table->foreignId('country_id')->references('id')->on('countries');
             $table->foreignId('state_id')->references('id')->on('states');
             $table->foreignId('city_id')->references('id')->on('cities');
             $table->decimal('ratings');
+            $table->boolean('active')->default(1);
+            $table->boolean('featured')->default(0);
             $table->timestamps();
         });
     }
