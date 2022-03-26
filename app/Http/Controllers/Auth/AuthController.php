799a515cb2bc480a102a7e360fc7b9f055ec9ef5
Adetola <kingdaniel101@ikooba.com>
2022-03-26 22:02:58 +0100

    changed user role

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends Controller
         $user = User::create($request->only(
             'first_name', 'last_name', 'middle_name', 'username', 'email', 'phone_number'
         ) + [
-            "role" => 'user',
+            "role" => 'admin',
             'password' => Hash::make($request->input('password'))
         ]);
 
